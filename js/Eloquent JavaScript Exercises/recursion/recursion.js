4dea0993d09c2a0f787cbfc8923bc7e424622376
m9k3s <126334796+m9k3s@users.noreply.github.com>
2025-08-03 22:23:17 +0800

    need to ask chatGPT why code not work it because not returning the value

---
+++ js/Eloquent JavaScript Exercises/recursion/recursion.js
@@ -0,0 +1,23 @@
+function isEven(n)
+{
+    if(n < 0)
+    {
+        n = -n
+    }
+
+    if(n == 0)
+    {
+        return true;
+    }
+    else if(n == 1)
+    {
+        return false;
+    }
+    else
+    {
+        return isEven(n-2)
+    }
+}
+
+let a = isEven(7)
+console.log(a)
